5b5ab21409c731dbc758db8d7b36234e40c8e810
KyberYbk <rechuta200344@gmail.com>
2023-05-13 01:56:20 +0600

    всякое
    Запилил отзывы, станицу пользователя, страницы для каждого бренда

--- app/Http/Controllers/PhoneBrand.php
+++ app/Http/Controllers/PhoneBrand.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\phonebrands;
+use App\Models\review;
 
 class PhoneBrand extends Controller
 {
-    public function main(){
-        $brands = phonebrands::OrderBy('id', 'desc')->get();
-        return view('main', ['brand'=>$brands]);
+
+    public function main()
+    {
+        $brand = phonebrands::OrderBy('id', 'desc')->get();  // Вывод брендов
+        $reviews = Review::OrderBy('id','desc')->get();                            // Вывод отзывов
+        return view('main', compact('brand', 'reviews'));
     }
 }
